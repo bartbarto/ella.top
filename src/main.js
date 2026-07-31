fa98292f9a7a24cfc3fd9c90ebff8e3f82bedab8
Bart <bartvanitterbeeck@gmail.com>
2026-07-31 11:03:22 +0200

    add animated interactive favicon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { startFaviconAnimation } from './favicon.js';
 import './style.css';
 
+startFaviconAnimation();
 createApp(App).mount('#app');
